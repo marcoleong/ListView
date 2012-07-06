f8e1c458bea14606c864be02065ca1486653829b
Marco Leong <marcogood411@gmail.com>
2012-07-07 00:49:56 +0800

    first development version committed

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-ListView
-========
+jQuery ListView
+===============
+
+This is a jQuery plugin doing similar things as iOS ListView but with HTML tag `<ul> <li>` as the data structure.
+This plugin script part is ready. But the css I provide is bad.
+
+Usage
+-----
+
+Markup
+
+```html
+<ul class="nestable-view">
+	<li>
+		Campus 1
+		<ul>
+			<li>Block A
+				<ul>
+					<li>Room A1</li>
+					<li>Room A2/li>
+					<li>Room A3</li>
+				</ul>
+			</li>
+			<li>Block B
+				<ul>
+					<li>Room B1</li>
+					<li>Room B2</li>
+					<li>Room B3</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li>
+		Campus 2
+		<ul>
+			<li>Block C
+				<ul>
+					<li>Room C1</li>
+					<li>Room C2</li>
+					<li>Room C3</li>
+				</ul>
+			</li>
+			<li>Block D</li>
+		</ul>
+	</li>
+</ul>
+```
+
+Add script
+
+```javascript
+$('.nestable-view').listView();
+```
+
+Add style
+
+```css
+.nestable-container {
+	overflow:hidden;
+	width:100px;
+	height:300px;
+	position:relative;
+	z-index:0;
+	background-color: white;
+	border:1px solid black;
+
+}
+.nestable-view {
+	/*overflow:hidden;*/
+	width:99px;
+}
+
+.nestable-view ul{
+	display:inline-block;
+	left:0px;
+	top:0px;
+	background-color:white;
+	width:100px;
+	height:300px;
+}
+
+ul.nestable-view,.nestable-view ul{
+	position:absolute;
+	padding:0;
+	margin:0;
+}
+
+.nestable-view li {
+	list-style:none;
+}
+
+.nestable-view li:hover {
+	background-color: yellow;
+}
+
+```
---
+++ jquery.listview-1.0b.js
@@ -0,0 +1,82 @@
+(function(){
+	$.fn.extend({
+		listView: function() {
+			//putting add left property for all ul
+			//when an children li is clicked, left -=100px, animated
+			this.initialize();
+		},
+
+		initialize: function(){
+			$(this).wrap('<div class="nestable-container"></div>');
+			//initialize left property
+			$(this).addClass('nestable-view');
+			var lvl = 0;
+			this.addLeftProperty($(this), lvl);
+
+			this.bindClickEvents($(this));
+		},
+
+		addLeftProperty: function(ele,lvl){
+
+			//check if it have element to work with
+			if(ele.length > 0){
+				ele.each(function(i,ele){
+					// li
+					$(ele).attr('data-level', lvl);
+				});
+
+				// all left except the first layer.
+				if(lvl !== 0){
+					$(ele).css({'left':'100px'});
+				}else{
+					$(ele).css({'left':'0px'});
+				}
+				lvl += 1;
+
+				// recursively add for every ul element
+				this.addLeftProperty($(ele).children().children(),lvl);
+			}else{
+				return false;
+			}
+		},
+
+		bindClickEvents: function(ele){
+			var root = $(this);
+			//check is there any node need to bind.
+			if(ele.children().length > 0){
+
+				// add a "Parent" button.
+				$(ele).each(function(i,ele){
+					$(ele).children().first().before('<li class="parent-btn">Parent</li>');
+					var pbtn = $(ele).children().first();
+					$(pbtn).click(function(e){
+						$(root).animate({'left':'+=100px'});
+						$(pbtn).parent().css({'z-index': '1'});
+
+						e.stopPropagation();
+					});
+				});
+
+				//for each children of an ul, means the li
+				$(ele).children().each(function(i,ele){
+
+					$(ele).click(function(e){
+						if($(ele).children().length > 0 && !$(ele).is('.parent-btn') ){
+							$(root).animate({'left':'-=100px'});
+							$(ele).children().css({'z-index':'1'});
+							$(ele).siblings().children().css({'z-index':'-1'});
+						}
+						e.stopPropagation();
+					});
+
+				});
+
+				// recursively bind click event.
+				this.bindClickEvents($(ele).children().children());
+			}else{
+				return false;
+			}
+
+		}
+	});
+})
